946d6fea09650913e349861aa45b7f3818b1ecf2
Christophe Boulanger <christophe.boulanger@imio.be>
2019-11-28 11:09:08 +0100

    Remove RichText out of procedure schema because we've got it thanks to behavior

--- src/cpskin/contenttypes/content/procedure.py
+++ src/cpskin/contenttypes/content/procedure.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.textfield import RichText
+from cpskin.locales import CPSkinMessageFactory as _
 from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope import schema
 from zope.interface import implementer
 
 
-from cpskin.locales import CPSkinMessageFactory as _
-
-
 class IProcedure(model.Schema):
     """ Marker interface and Dexterity Python Schema for Procedure
     """
 
-    e_guichet = schema.TextLine(
-        title=_(u'E-Guichet'),
-        required=False
-    )
-
-    text = RichText(
-        title=_(u'Text'),
-        required=False
-    )
+    e_guichet = schema.TextLine(title=_(u"E-Guichet"), required=False)
 
 
 @implementer(IProcedure)
