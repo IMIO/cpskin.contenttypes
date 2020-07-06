113da52de9e5b7ede72be8e12adeddc56138724b
Christophe Boulanger <christophe.boulanger@imio.be>
2020-07-06 14:23:39 +0200

    Back to development: 1.0.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='cpskin.contenttypes',
-    version='1.0.7',
+    version='1.0.8.dev0',
     description="CPSkin content types",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
