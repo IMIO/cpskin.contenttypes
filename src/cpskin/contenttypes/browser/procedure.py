d6b28ee98383169115b8068f222285063730f109
Christophe Boulanger <christophe.boulanger@imio.be>
2020-07-06 13:13:06 +0200

    Custom EditForm, custom AddForm to hide e_guichet field when ITsProcedure behavior is there.

--- src/cpskin/contenttypes/browser/procedure.py
+++ src/cpskin/contenttypes/browser/procedure.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from imio.behavior.teleservices.behaviors.ts_procedure import ITsProcedure
+from plone.app.contenttypes.behaviors.leadimage import ILeadImage
+from plone.dexterity.browser import add
+from plone.dexterity.browser import edit
+from plone.dexterity.interfaces import IDexterityFTI
 from Products.Five import BrowserView
+from zope.component import queryUtility
 
 
 class ProcedureView(BrowserView):
@@ -17,3 +22,35 @@ class ProcedureView(BrowserView):
             return False
         else:
             return True
+class ProcedureEditForm(edit.DefaultEditForm):
+    # if behavior is install so we hide default e_guichet field.
+    def updateWidgets(self):
+        super(ProcedureEditForm, self).updateWidgets()
+        if ITsProcedure.providedBy(self.context):
+            self.widgets['e_guichet'].mode = 'hidden'
+
+
+class CustomAddForm(add.DefaultAddForm):
+    portal_type = 'Procedure'
+
+    def updateWidgets(self):
+        super(CustomAddForm, self).updateWidgets()
+        # When we add new contentype object, our behavior doesn't exist yet
+        # So we search ITsProcedure in FTI
+        if self.has_behavior("Procedure", "imio.behavior.teleservices.behaviors.ts_procedure.ITsProcedure"):
+            self.widgets['e_guichet'].mode = 'hidden'
+
+    def has_behavior(self, type_name, behavior_name):
+        """Check if a behavior is on portal_type"""
+        fti = queryUtility(IDexterityFTI, name=type_name)
+        if not fti:
+            return
+        behaviors = list(fti.behaviors)
+        if behavior_name not in behaviors:
+            return False
+        else:
+            return True
+
+
+class AddView(add.DefaultAddView):
+    form = CustomAddForm
